c2369bb42aa9629152a83ade024b953569c83e5e
Kirill Suslov <euchekavelo@gmail.com>
2025-04-06 13:57:29 +0300

    Added project basic structure.

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "custom-thread-pool-service"
+
---
+++ src/main/java/ru/mephi/customthreadpoolservice/Main.java
@@ -0,0 +1,7 @@
+package ru.mephi.customthreadpoolservice;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
